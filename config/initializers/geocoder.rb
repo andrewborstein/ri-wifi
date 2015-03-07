81ba85f0a69c43480ccba53e992d336fe9b20b1c
Andrew Borstein <info@brownborgstudios.com>
2015-03-07 13:48:30 -0500

    Geocoder config - timeout only

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,10 +1,10 @@
 Geocoder.configure(
 
   # geocoding service (see below for supported options):
-  :lookup => :yandex,
+  # :lookup => :yandex,
 
   # IP address geocoding service (see below for supported options):
-  :ip_lookup => :maxmind,
+  # :ip_lookup => :maxmind,
 
   # geocoding service request timeout, in seconds (default 3):
   :timeout => 10,
